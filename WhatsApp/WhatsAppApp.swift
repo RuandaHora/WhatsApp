55db728e32282c00d9789b1d68595cd8b608a559
Ruan da Hora <ruanpablosnake@gmail.com>
2023-03-29 21:02:15 -0300

    Initial Commit

---
+++ WhatsApp/WhatsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsAppApp.swift
+//  WhatsApp
+//
+//  Created by Marcelo Hora on 29/03/23.
+//
+
+import SwiftUI
+
+@main
+struct WhatsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
